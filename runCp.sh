3e92eb14ea705f1220789b12fb4144725fc4930f
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2018-06-06 10:57:26 +1000

    added padding to sequence

--- runCp.sh
+++ runCp.sh
@@ -3,10 +3,10 @@
 ## This script runs changePoint for a number of segment classes. (Hard coded 2 to 20)
 ## input file name is hard coded
 
-nums=$(seq 2 20)
+nums=$(seq -w 2 20)
 
 for i in $nums
 do
-	changePoint -i INPUTFILENAME -o OUTPUFILENAMEng$i -n 1000 -ng $i 	
+	changePoint -i INPUTFILENAME -o OUTPUFILENAMEng$i -n 1000 -ng $i -s 10000	
 done
 
