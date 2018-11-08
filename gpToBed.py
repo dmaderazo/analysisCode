85bc37ed6f301980d32ff89ae1c761271e344a6c
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2018-11-08 16:14:45 +1100

    Finished file is now able to generate .bed files for comparison

--- gpToBed.py
+++ gpToBed.py
@@ -23,6 +23,8 @@ segSize = 50 # Set this to be length of a seq in output file
 t = args.threshold
 
 ## Remove any temp giles that are present in the directory from previous analysis
+if os.path.isfile(args.output) == True:
+	os.remove(args.output)
 if os.path.isfile('maf_gp_temp') == True:
     os.remove('maf_gp_temp')
 if os.path.isfile('human_info_temp') == True:
@@ -35,28 +37,28 @@ subprocess.call('touch maf_gp_temp',shell=True)
 ## format:
 ## no. chrom_in_ref pos_start pos_end other_org pos_start pos_end seq_len
 
-with open(args.mafFile) as f:
-	with open('maf_gp_temp','w+') as g:
-	    for dirtyProfileLine in f:
-	    	cleanProfileLine = dirtyProfileLine.split()
-	    	if len(cleanProfileLine) <= 2:
-	    		pass
-	    	elif 'hg19'in cleanProfileLine[1]:
-	    		seqLen = int(cleanProfileLine[3])
-	    		if seqLen > segSize:
-	    			q = seqLen/segSize
-	    			r = seqLen % segSize
-	    			for i in range(1,q+1):
-	    				seqStart = int(cleanProfileLine[2])+(i-1)*segSize
-	    				writeString = "{},{},{}\n".format(cleanProfileLine[1],seqStart,segSize)
-	    				g.write(writeString)
-	    				if i == max(range(q+1)) and r != 0:
-	    					seqStart = seqStart+segSize
-	    					writeString = "{},{},{}\n".format(cleanProfileLine[1],seqStart,r)
-	    					g.write(writeString)
-	    		else:
-	    			writeString = "{},{},{}\n".format(cleanProfileLine[1],cleanProfileLine[2],cleanProfileLine[3])
-	    			g.write(writeString)
+# with open(args.mafFile) as f:
+# 	with open('maf_gp_temp','w+') as g:
+# 	    for dirtyProfileLine in f:
+# 	    	cleanProfileLine = dirtyProfileLine.split()
+# 	    	if len(cleanProfileLine) <= 2:
+# 	    		pass
+# 	    	elif 'hg19'in cleanProfileLine[1]:
+# 	    		seqLen = int(cleanProfileLine[3])
+# 	    		if seqLen > segSize:
+# 	    			q = seqLen/segSize
+# 	    			r = seqLen % segSize
+# 	    			for i in range(1,q+1):
+# 	    				seqStart = int(cleanProfileLine[2])+(i-1)*segSize
+# 	    				writeString = "{},{},{}\n".format(cleanProfileLine[1],seqStart,segSize)
+# 	    				g.write(writeString)
+# 	    				if i == max(range(q+1)) and r != 0:
+# 	    					seqStart = seqStart+segSize
+# 	    					writeString = "{},{},{}\n".format(cleanProfileLine[1],seqStart,r)
+# 	    					g.write(writeString)
+# 	    		else:
+# 	    			writeString = "{},{},{}\n".format(cleanProfileLine[1],cleanProfileLine[2],cleanProfileLine[3])
+# 	    			g.write(writeString)
 
 ## Generate a file from .maf that only contains the human information
 with open(args.mafFile, 'r') as f:
@@ -69,98 +71,86 @@ with open(args.mafFile, 'r') as f:
 with open('human_info_temp','r') as f:
 	with open(args.groupFile,'r') as g:
 		with open(args.output,'w+') as h: 
-			for line in f: #get human data
-				dirtyData = line.rstrip().split()
-				# print dirtyData
-				speciesChorom = dirtyData[1]
-				chromStart = int(dirtyData[2])
-				for line in g: #corresponds to an alignment block
-					# produce an np array of numeric
-					valueVec = line.rstrip().split(',')
-					valueVec = valueVec[1:]
-					valueVec = np.array(map(float,valueVec))
+			for line in g: #corresponds to an alignment block
 
-					#find positions that are above threshold
-					indexVec = np.where(valueVec > t)[0]
-					diffVec = np.diff(indexVec)
-					#find positions where gaps are in the block
-					gapLocation = np.where(diffVec > 6)[0]
+				# produce an np array of numeric
+				valueVec = line.rstrip().split(',')
+				valueVec = valueVec[1:]
+				valueVec = np.array(map(float,valueVec))
+
+				#find positions that are above threshold
+				indexVec = np.where(valueVec > t)[0]
+				diffVec = np.diff(indexVec)
+				#find positions where gaps are in the block
+				gapLocation = np.where(diffVec > 6)[0]
+				
+				if len(indexVec) == 0:
+					pass
+					#no segments above threshold in this line. Need to advance human data.dirtyData
+					dirtyData = f.readline().rstrip().split()
+
+				elif len(gapLocation) == 0:
+					#only one segment in the line
+					#positions of segment starts
+					segStart = np.insert(indexVec[gapLocation + 1],0,0)
+					#positions of segment ends
+					segEnd = np.insert(indexVec[gapLocation],len(indexVec[gapLocation]),max(indexVec))
+
+					dirtyData = f.readline().rstrip().split()
+
+					speciesChorom = dirtyData[1]
+					tempVar1 = speciesChorom.split('.')
+					chromStart = int(dirtyData[2])
+					justChrom = tempVar1[1]
 					
-					# if there are gaps => multiple segments in the class
-					if len(gapLocation) > 0:
-						#positions of segment starts
-						segStart = np.insert(indexVec[gapLocation + 1],0,0)
-						#positions of segment ends
-						segEnd = np.insert(indexVec[gapLocation],len(indexVec[gapLocation]),max(indexVec))
-						delVec = np.array([])	
 
-						# Collect positions for segments that are <6 in len
-						for i in range(len(segStart)):
-							segLen = segEnd[i] - segStart[i]
-							if segLen > 50:
-								print 'segment size = {}'.format(segLen)
-							if segLen < 6:
-								delVec = np.insert(delVec,0,i)
-						# delete those segments
-						delVec = delVec.astype(int)
-						segStart = np.delete(segStart,delVec)
-						segEnd = np.delete(segEnd,delVec)
-						
-						for j in range(len(segStart)):
-							bedChromStart = chromStart + segStart[j]
-							bedChromEnd = chromStart + segEnd[j] + 1 # +1 because interval is [start,end)
-							writeString = '{} {} {}\n'.format(speciesChorom,bedChromStart,bedChromEnd)
+					bedChromStart = chromStart  + segStart[0]
+					bedChromEnd = chromStart + segEnd[0] + 1
+					
+					writeString = '{} {} {}\n'.format(justChrom,bedChromStart,bedChromEnd)
+					h.write(writeString)
 
-							h.write(writeString)
+				# # if there are gaps => multiple segments in the class
+				elif len(gapLocation) > 0:
+					
+					# print 'more than one segment in block \n'
+					#positions of segment starts
+					segStart = np.insert(indexVec[gapLocation + 1],0,0)
+					#positions of segment ends
+					segEnd = np.insert(indexVec[gapLocation],len(indexVec[gapLocation]),max(indexVec))
+					delVec = np.array([])	
 
+					# Collect positions for segments that are <6 in len
+					for i in range(len(segStart)):
+						segLen = segEnd[i] - segStart[i]
+						# if segLen > 50:
+						# 	print 'segment size = {}'.format(segLen)
+						if segLen < 6:
+							delVec = np.insert(delVec,0,i)
+					# delete those segments
+					delVec = delVec.astype(int)
+					segStart = np.delete(segStart,delVec)
+					segEnd = np.delete(segEnd,delVec)
+
+					dirtyData = f.readline().rstrip().split()
+					speciesChorom = dirtyData[1]
+					tempVar1 = speciesChorom.split('.')
+					justChrom = tempVar1[1]
+					chromStart = int(dirtyData[2])
+					
+					for j in range(len(segStart)):
+						bedChromStart = chromStart + segStart[j]
+						bedChromEnd = chromStart + segEnd[j] + 1 # +1 because interval is [start,end)
+						writeString = '{} {} {}\n'.format(justChrom,bedChromStart,bedChromEnd)
+
+						h.write(writeString)
+
+						# print writeString
+
+command = 'sort -o {} {}'.format(args.output,args.output)
+subprocess.call(command,shell=True)
 ## Delete temp files		
 # uncomment this section at the end 
-# os.remove('human_info_temp')
-# os.remove('maf_gp_temp')
+os.remove('human_info_temp')
+os.remove('maf_gp_temp')
 
-	    		# writeString = "{},{},{}\n".format(cleanProfileLine[1],cleanProfileLine[2],cleanProfileLine[3])
-    			# g.write(writeString)
-        # species = lines[1]
-        # if 'hg19' in species:
-        	# print('yes')
-                # print(lines[0])
-                # cleanLines = lines.split(' ')
-                # fleeb.write(lines)
-
-# create file with sequence positions in ref organism (just numbers)
-# with open('tempFile','r') as thing:
-#     with open('thisTest','w+') as blah:
-#         for line in thing:
-#             axtLine = line.split()
-#             startNum = int(axtLine[2])
-#             endNum = int(axtLine[3])
-#             sequencePositions = range(startNum,endNum + 1)
-#             seqPos = ','.join(map(repr,sequencePositions))
-
-
-#             blah.write('{}\n'.format(seqPos))
-
-
-#writes a wiggle track 
-#format
-#Seq_pos profile value (how to filter?)
-# with open('thisTest','r') as f:
-#     with open(args.groupfile, 'r') as g:
-#         with open(args.output,'w+') as h:
-#             gpData = csv.reader(g)
-#             seqPos = csv.reader(f)
-            
-#             for line1,line2 in zip(gpData,seqPos):
-#                 cleanLines = np.array(np.float_(line1[1:]))
-#                 seqPosPre = np.array(np.int32(line2))
-#                 indeces = np.where(cleanLines > args.threshold) 
-#                 if len(indeces[0]) == 0:
-#                     pass
-#                 elif len(indeces[0]) < 101:
-#                     pass
-#                 else:                    
-#                     profVal = cleanLines[indeces[0]]
-#                     seqPosFinal = seqPosPre[indeces[0]]
-#                     for a,b in zip(seqPosFinal,profVal):
-#                         s = '{} {}\n'.format(a,b)
-#                         h.write(s)
