a1d07d2610aa951b765460bf932683622c9a5be2
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2018-12-11 15:09:53 +1100

    added encoding functions. still needs work

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@
 import os 
 import subprocess
 import numpy as np
+import argparse
+
 # Encoding function
 
 # def encoding(inputFile,outputFile):
@@ -16,6 +18,175 @@ import numpy as np
 # 		#Do 3 way
 # 	else:
 # 		print 'Error: Check input file types are .net.axt or .maf.'
+# function to identify proportion of string made up of elements of bad letters 
+def char_prop(word):
+    # THIS IS CASE SENSITIVE
+    # These letters are deifinitely ignored by changepoint
+    badSet = {'I','J','K','L','M','N','#'}
+    word_len = float(len(word))
+    char_count = 0
+
+    for c in word:
+        if c in badSet:
+            char_count += 1
+
+    return (char_count/word_len) 
+def encoding_2_way(org1,org2):
+	outSeq = ''
+    for c,d in zip(org1,org2):
+        if c == '-':
+            # remove gaps
+            pass
+        elif c == 'A' and d == 'A':
+            outSeq += 'a'
+        elif c == 'A' and d == 'C':
+            outSeq += 'b'
+        elif c == 'A' and d == 'G':
+            outSeq += 'c'
+        elif c == 'A' and d == 'T':
+            outSeq += 'd'
+        elif c == 'C' and d == 'A':
+            outSeq += 'e'
+        elif c == 'C' and d == 'C':
+            outSeq += 'f'
+        elif c == 'C' and d == 'G':
+            outSeq += 'g'
+        elif c == 'C' and d == 'T':
+            outSeq += 'h'
+        elif c == 'G' and d == 'A':
+            outSeq += 'i'
+        elif c == 'G' and d == 'C':
+            outSeq += 'j'
+        elif c == 'G' and d == 'G':
+            outSeq += 'k'
+        elif c == 'G' and d == 'T':
+            outSeq += 'l'
+        elif c == 'T' and d == 'A':
+            outSeq += 'm'
+        elif c == 'T' and d == 'C':
+            outSeq += 'n'
+        elif c == 'T' and d == 'G':
+            outSeq += 'o'
+        elif c == 'T' and d == 'T':
+            outSeq += 'p'    
+        elif d == '-':
+            outSeq += 'I'
+    outSeq += '#'
+    return outSeq
+
+def encoding_3_way(org1,org2,org3):
+    #Define our bad letters
+    badSet = {'I','J','K','L','M','N'}
+   
+    outSeq = ''
+    for c,d,e in zip(org1,org2,org3):
+        if c == '-' and d == '-' and e =='-': #(000)
+            pass
+        elif c != '-' and d == '-' and e =='-':
+            outSeq += 'N' #(100)
+        elif c != '-' and d == '-' and e !='-':
+            outSeq += 'L' #(101)
+        elif c != '-' and d != '-' and e =='-':
+            outSeq += 'K' #(110)
+        elif c == '-' and d != '-' and e !='-':
+            outSeq += 'J' #(011)
+        elif c == '-' and d == '-' and e !='-':
+            outSeq += 'M' #(001)
+        elif c == '-' and d != '-' and e =='-':
+            outSeq += 'I'   #(010)
+        elif (c == 'A' and d == 'A' and e == 'A') or (c == 'T' and d == 'T' and e == 'T'):
+            outSeq += 'a'
+        elif (c == 'A' and d == 'A' and e == 'C') or (c == 'T' and d == 'T' and e == 'G'):
+            outSeq += 'b'
+        elif (c == 'A' and d == 'A' and e == 'G') or (c == 'T' and d == 'T' and e == 'C'):
+            outSeq += 'c'
+        elif (c == 'A' and d == 'A' and e == 'T') or (c == 'T' and d == 'T' and e == 'A'):
+            outSeq += 'd'
+        elif (c == 'A' and d == 'C' and e == 'A') or (c == 'T' and d == 'G' and e == 'T'):
+            outSeq += 'e'
+        elif (c == 'A' and d == 'C' and e == 'C') or (c == 'T' and d == 'G' and e == 'G'):
+            outSeq += 'f'
+        elif (c == 'A' and d == 'C' and e == 'G') or (c == 'T' and d == 'G' and e == 'C'):
+            outSeq += 'g'
+        elif (c == 'A' and d == 'C' and e == 'T') or (c == 'T' and d == 'G' and e == 'A'):
+            outSeq += 'h'
+        elif (c == 'A' and d == 'G' and e == 'A') or (c == 'T' and d == 'C' and e == 'T'):
+            outSeq += 'i'
+        elif (c == 'A' and d == 'G' and e == 'C') or (c == 'T' and d == 'C' and e == 'G'):
+            outSeq += 'j'
+        elif (c == 'A' and d == 'G' and e == 'G') or (c == 'T' and d == 'C' and e == 'C'):
+            outSeq += 'k'
+        elif (c == 'A' and d == 'G' and e == 'T') or (c == 'T' and d == 'C' and e == 'A'):
+            outSeq += 'l'
+        elif (c == 'A' and d == 'T' and e == 'A') or (c == 'T' and d == 'A' and e == 'T'):
+            outSeq += 'm'
+        elif (c == 'A' and d == 'T' and e == 'C') or (c == 'T' and d == 'T' and e == 'G'):
+            outSeq += 'n'
+        elif (c == 'A' and d == 'T' and e == 'G') or (c == 'T' and d == 'A' and e == 'C'):
+            outSeq += 'o'
+        elif (c == 'A' and d == 'T' and e == 'T') or (c == 'T' and d == 'A' and e == 'A'):
+            outSeq += 'p'
+        elif (c == 'C' and d == 'A' and e == 'A') or (c == 'G' and d == 'T' and e == 'T'):
+            outSeq += 'q'
+        elif (c == 'C' and d == 'A' and e == 'C') or (c == 'G' and d == 'T' and e == 'G'):
+            outSeq += 'r'
+        elif (c == 'C' and d == 'A' and e == 'G') or (c == 'G' and d == 'T' and e == 'C'):
+            outSeq += 's'
+        elif (c == 'C' and d == 'A' and e == 'T') or (c == 'G' and d == 'T' and e == 'A'):
+            outSeq += 't'
+        elif (c == 'C' and d == 'C' and e == 'A') or (c == 'G' and d == 'G' and e == 'T'):
+            outSeq += 'u'
+        elif (c == 'C' and d == 'C' and e == 'C') or (c == 'G' and d == 'G' and e == 'G'):
+            outSeq += 'v'
+        elif (c == 'C' and d == 'C' and e == 'G') or (c == 'G' and d == 'G' and e == 'C'):
+            outSeq += 'w'
+        elif (c == 'C' and d == 'C' and e == 'T') or (c == 'G' and d == 'G' and e == 'A'):
+            outSeq += 'x'
+        elif (c == 'C' and d == 'G' and e == 'A') or (c == 'G' and d == 'C' and e == 'T'):
+            outSeq += 'y'
+        elif (c == 'C' and d == 'G' and e == 'C') or (c == 'G' and d == 'C' and e == 'C'):
+            outSeq += 'z' 
+        elif (c == 'C' and d == 'G' and e == 'G') or (c == 'G' and d == 'C' and e == 'C'):
+            outSeq += 'U'
+        elif (c == 'C' and d == 'G' and e == 'T') or (c == 'G' and d == 'C' and e == 'A'):
+            outSeq += 'V'
+        elif (c == 'C' and d == 'T' and e == 'A') or (c == 'G' and d == 'A' and e == 'T'):
+            outSeq += 'W'
+        elif (c == 'C' and d == 'T' and e == 'C') or (c == 'G' and d == 'A' and e == 'G'):
+            outSeq += 'X'
+        elif (c == 'C' and d == 'T' and e == 'G') or (c == 'G' and d == 'A' and e == 'C'):
+            outSeq += 'Y'
+        elif (c == 'C' and d == 'T' and e == 'T') or (c == 'G' and d == 'A' and e == 'A'):
+            outSeq += 'Z'
+
+    wordSet = set(outSeq)
+
+    #if the word is made up entirely of bad characters discard 
+    if wordSet <= badSet:
+        return None
+    #This is necessary for change point input.
+
+    # if the word is too short discard
+    ## ASK JON ABOUT THIS. Do I do this before or after?
+    elif len(outSeq) < 7:
+       return None
+    # if the word is made up of mostly gaps, discard
+    #elif char_prop(outSeq) > 0.5:
+     #   return None
+    else: #return outSeq
+        return outSeq
+
+def encoding(inputFile,outputFile):
+	if os.path.isfile(outputFile) == False:
+    	pass
+	else:
+    	os.remove(outputFile)
+
+    if os.path.isfile(newBed) == False:
+    	pass
+	else:
+    	os.remove(newBed)
+
 
 def file_len(fname):
     p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, 
