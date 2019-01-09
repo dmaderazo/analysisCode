7f4a42afe827a183a3af313f867c7bc4f5e40bc0
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2019-01-09 15:21:39 +1100

    I think this is working

--- runJobs.sh
+++ runJobs.sh
@@ -6,24 +6,29 @@
 # This script assumes that the following has been run successfully:
 # scons co=1 b=GccOpt ts=projects/TanCollaboration/test/TestCryptInvasion.hpp
 #
-
 cd ../../../
-
 initial_sim=2;
 offset=0;
 num_runs=1;
-num_sweeps=10;
-final_sim=20;
-
-for start_sim in $(seq -w $initial_sim $final_sim);
+num_sweeps=10; #This number of sims starting from $initial_sim
+for (( i=0 ; i<${num_sweeps} ; i++))
 do
-    start_sim=`expr $i \* $num_runs + $initial_sim`;
+    temp_num=`expr $i \* $num_runs + $initial_sim`;
     #offset_num_runs=`expr $num_runs-$offset`;
-    echo $start_sim
-    nice -20 changePoint -i chr1Encoded  -n 1000 -b 1000 -s 100000 -ng $start_sim -o seg_ng_$start_sim > ./outMessages/stdoutseg_ng_$start_sim.txt & 
+
+    if [  $temp_num -lt 10 ]
+    then
+    	pad="0"
+    	sim_num="$pad$temp_num"
+    else
+    	sim_num=$temp_num
+    fi
+
+    echo $sim_num
+
+    #nice -20 changePoint -i chr1Encoded  -n 1000 -b 1000 -s 100000 -ng  sim_num -o seg_ng_$sim_num > ./outMessages/stdoutseg_ng_$sim_num.txt & 
  
 done
 
 echo "Jobs submitted"
-
 
