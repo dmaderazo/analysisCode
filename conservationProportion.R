b635cfbacb53518bbb39e2238af08efe5657bdc1
Dominic Maderazo <dmaderazo@student.unimelb.edu.au>
2019-01-21 15:22:26 +1100

    script to identify the most conserved segment class

---
+++ conservationProportion.R
@@ -0,0 +1,17 @@
+
+# script assumes that 3wayGetCharFreqs has been run
+# Required for arrange function
+library(tidyverse)
+
+gpCharacterFreqs <- read.csv('gpData.csv',header=TRUE) #substitute your output file from 3wayGetCharFreqs
+
+# Names of relevant columns
+consNames <- c('X000gpNum','a','v')
+
+consDataFrame <- gpCharacterFreqs[consNames]
+
+consDataFrame$consProp <- NA
+
+consDataFrame['consProp'] <- consDataFrame['a'] + consDataFrame['v']
+
+consDataFrame %>% arrange(desc(consProp))
